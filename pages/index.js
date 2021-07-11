d7a1cf5a6455ecbecce91295dd25ab018ca3d911
amouryamina3 <amouryamina3@gmail.com>
2021-07-11 18:21:29 +0200

    consomme l'api

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Layout from "./components/layout";
 
 export default function Home({ res }) {
@@ -9,7 +10,64 @@ export default function Home({ res }) {
           <li
             key={index}
             className="relative hover:shadow-md p-8 border border-blue-300 rounded-3xl bg-blue-100 md:w-auto flex-1 mx-5"
-          ></li>
+          >
+            <Link href="/">
+              <a className="rounded-md">
+                <div className="text-center">
+                  <img
+                    src={crypto.logo_url}
+                    alt={crypto.name}
+                    className="w-20 h-20 mx-auto mb-6"
+                  />
+                </div>
+                <h2 className="text-2xl mb-6 uppercase tracking-wider">
+                  {crypto.name}
+                </h2>
+                <h3 className="font-bold text-2xl mb-4">
+                  {parseFloat(crypto.price).toFixed(2)} USD
+                </h3>
+                <p>
+                  1 jour :{" "}
+                  <span className="font-bold">
+                    {parseFloat(crypto["1d"].price_change_pct * 100).toFixed(
+                      2
+                    ) + "%"}
+                  </span>
+                  {crypto["1d"].price_change_pct < 0 ? (
+                    <span className="text-red-500"> &#x2798;</span>
+                  ) : (
+                    <span className="text-green-500"> &#x279A;</span>
+                  )}
+                </p>
+                <p>
+                  1 mois :{" "}
+                  <span className="font-bold">
+                    {parseFloat(crypto["30d"].price_change_pct * 100).toFixed(
+                      2
+                    ) + "%"}
+                  </span>
+                  {crypto["30d"].price_change_pct < 0 ? (
+                    <span className="text-red-500"> &#x2798;</span>
+                  ) : (
+                    <span className="text-green-500"> &#x279A;</span>
+                  )}
+                </p>
+                <p>
+                  1 an :{" "}
+                  <span className="font-bold">
+                    {parseFloat(crypto["365d"].price_change_pct * 100).toFixed(
+                      2
+                    ) + "%"}
+                  </span>
+                  {crypto["365d"].price_change_pct < 0 ? (
+                    <span className="text-red-500"> &#x2798;</span>
+                  ) : (
+                    <span className="text-green-500"> &#x279A;</span>
+                  )}
+                </p>
+              </a>
+            </Link>
+          </li>
         ))}
       </ul>
     </Layout>
